9dbe91fa5cb772eaf65fd039b7e9a4b9eb850666
Skulltulaidm <80136971+Skulltulaidm@users.noreply.github.com>
2023-09-15 00:09:27 +0000

    add and update

---
+++ monex.py
@@ -0,0 +1,35 @@
+import re
+import pdfplumber
+import pandas as pd
+
+def process_pdf(uploaded_file):
+    """Procesa un archivo PDF de Monex y devuelve un DataFrame de Pandas."""
+
+    def extract_pdf_text(file):
+        all_text = ""
+        with pdfplumber.open(file) as pdf:
+            for page in pdf.pages:
+                all_text += page.extract_text() + "\n"
+        return all_text
+
+    all_text = extract_pdf_text(uploaded_file)
+    pattern = re.compile(r'(\d{2}/\w{3})\s+([\w\s]+?)(?:\s+(\d{8}))?\s+(\d+\.\d{2}|\d{1,3}(?:,\d{3})*\.\d{2})\s+(\d+\.\d{2}|\d{1,3}(?:,\d{3})*\.\d{2})\s+(\d+\.\d{2}|\d{1,3}(?:,\d{3})*\.\d{2})\s+(\d+\.\d{2}|\d{1,3}(?:,\d{3})*\.\d{2})\s+(-?\d+\.\d{2}|-?\d{1,3}(?:,\d{3})*\.\d{2})\s+(-?\d+\.\d{2}|-?\d{1,3}(?:,\d{3})*\.\d{2})')
+    
+    matches = pattern.findall(all_text)
+
+    data = []
+
+    for match in matches:
+        data.append({
+            'Fecha': match[0],
+            'Descripción': match[1],
+            'Referencia': match[2],
+            'Abonos': match[3],
+            'Cargos': match[4],
+            'Movimiento Garantía': match[5],
+            'Saldo No Disponible': match[6],
+            'Saldo Disponible': match[7],
+            'Saldo Total': match[8]
+        })
+
+    return pd.DataFrame(data)
