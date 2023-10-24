c3cd3e426d92b2154520ce2201c4b824cf201114
Skulltulaidm <80136971+Skulltulaidm@users.noreply.github.com>
2023-10-24 19:34:35 +0000

    update banregio

--- banregio.py
+++ banregio.py
@@ -1,41 +1,41 @@
 import re
-import pandas as pd
 import pdfplumber
+import pandas as pd
 
 def process_pdf(uploaded_file):
     """Procesa un archivo PDF y devuelve un DataFrame de Pandas."""
-
-
-
+    
+    PATTERN = re.compile(
+        r'(\d{2})\s+'  # Dia
+        r'([A-Z]{3})\s+'  # COD. TRANSACC
+        r'(.+?)\s+'  # CONCEPTO
+        r'(\d{1,3}(?:,\d{3})*\.\d{2})?\s+'  # CARGO/ABONO
+        r'(\d{1,3}(?:,\d{3})*\.\d{2})?\s+'  # SALDO
+    )
+    CARGO_KEYWORDS = ['Cobro de cheque']
+    ABONO_KEYWORDS = ['DEPOSITO']
+    
     def extract_pdf_text(file):
         all_text = ""
         with pdfplumber.open(file) as pdf:
             for page in pdf.pages:
                 all_text += page.extract_text() + "\n"
         return all_text
+    
+    def preprocess_text(text):
+        # Esta función se puede expandir para futuras necesidades de limpieza/preprocesamiento
+        return text
 
-    def find_matches(text):
-        pattern = re.compile(
-            r'(\d{2})\s+'  # Dia
-            r'([A-Z]{3})\s+'  # COD. TRANSACC
-            r'(.+?)\s+'  # CONCEPTO
-            r'(\d{1,3}(?:,\d{3})*\.\d{2})?\s+'  # CARGO/ABONO
-            r'(\d{1,3}(?:,\d{3})*\.\d{2})?\s+'  # SALDO
-        )
-        return pattern.findall(text)
-
-    def create_dataframe(matches):
-
+    def extract_data(all_text):
+        matches = PATTERN.findall(all_text)
+        data = []
         date_match = re.search(r'(\d{2})\s+al\s+(\d{2})\s+de\s+([A-Z]+)\s+(\d{4})', all_text)
         if date_match:
             start_day, end_day, month, year = date_match.groups()
-            date_suffix = f"{month[:3]}/{year[2:]}"
-        data = []
+
         for match in matches:
             cod_transacc = match[1]
             cantidad = match[3]
-            CARGO_KEYWORDS = ['Cobro de cheque']
-            ABONO_KEYWORDS = ['DEPOSITO']
 
             if "ABONO" in match[2]:
                 abono = cantidad
@@ -48,7 +48,6 @@ def process_pdf(uploaded_file):
                 abono = cantidad if abono_condition else '0'
 
             dia = f"{match[0]}/{month[:3]}/{year[2:]}"
-
             data.append({
                 'DIA': dia, 
                 'COD. TRANSACC.': match[1],
@@ -57,9 +56,22 @@ def process_pdf(uploaded_file):
                 'ABONO': abono,
                 'SALDO': match[4]
             })
-        return pd.DataFrame(data)
+        return data
 
     all_text = extract_pdf_text(uploaded_file)
-    matches = find_matches(all_text)
-    df = create_dataframe(matches)
-    return df
+    all_text = preprocess_text(all_text)
+    
+    data = extract_data(all_text)
+
+    df = pd.DataFrame(data)
+
+    df['CARGO'] = pd.to_numeric(df['CARGO'].str.replace(',', '').astype(float), errors='coerce')
+    df['ABONO'] = pd.to_numeric(df['ABONO'].str.replace(',', '').astype(float), errors='coerce')
+
+    total_cargo = df['CARGO'][df['CARGO'] > 0].sum()
+    total_abono = df['ABONO'][df['ABONO'] > 0].sum()
+
+    count_cargo = df['CARGO'][df['CARGO'] > 0].count()
+    count_abono = df['ABONO'][df['ABONO'] > 0].count()
+
+    return df, total_cargo, total_abono, count_cargo, count_abono
