2bdb8042db8d76d4cff506edaa820c38ecf7d695
Skulltulaidm <80136971+Skulltulaidm@users.noreply.github.com>
2023-10-25 20:54:19 +0000

    update

--- app.py
+++ app.py
@@ -31,26 +31,24 @@ if uploaded_file is not None:
     
     if option == 'Banregio':
         df, total_cargos, total_abonos, count_cargo, count_abono = banregio.process_pdf(uploaded_file) 
-        st.write("Es probable que la última línea del Excel arroje información random del estado de cuenta, pero está fuera de los movimientos del mes.")  
         st.write(f'Total de Cargos: ${total_cargos:.2f}')
-        st.write(f'Total de Abonos: ${total_abonos:.2f}')  
+        st.write(f'Total de Abonos: ${total_abonos:.2f}')
         st.write(f'Número de Cargos: {count_cargo}')
-        st.write(f'Número de Abonos: {count_abono}') 
+        st.write(f'Número de Abonos: {count_abono}')
     elif option == 'Banbajio':
         df = banbajio.process_pdf(uploaded_file)
     elif option == 'Monex':
         df = monex.process_pdf(uploaded_file)
     elif option == 'Banorte-1':
-        df = banorteFormatoDesorden.process_pdf(uploaded_file) 
+        df = banorteFormatoDesorden.process_pdf(uploaded_file)
     elif option == 'Banorte-2':
         df = banorteFormato2.process_pdf(uploaded_file)
     elif option == 'Santander':
-        df_flexible, total_retiro, total_deposito, count_retiro, count_deposito = santander.process_pdf(uploaded_file) 
-        st.write("Es probable que la última línea del Excel arroje información random del estado de cuenta, pero está fuera de los movimientos del mes.")  
+        df_flexible, total_retiro, total_deposito, count_retiro, count_deposito = santander.process_pdf(uploaded_file)
         st.write(f'Total de Cargos: ${total_retiro:.2f}')
-        st.write(f'Total de Abonos: ${total_deposito:.2f}')  
+        st.write(f'Total de Abonos: ${total_deposito:.2f}')
         st.write(f'Número de Cargos: {count_retiro}')
-        st.write(f'Número de Abonos: {count_deposito}')  
+        st.write(f'Número de Abonos: {count_deposito}')
     elif option == 'Banamex':
         df, total_retiros, total_depositos = banamex.process_pdf(uploaded_file) 
         st.write("Este código funciona para ambos formatos de Banamex")  
